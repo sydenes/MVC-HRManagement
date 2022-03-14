910fa2bb174aff7491550ad06bbabf3e0072f14f
sydenes <s.enessagban@gmail.com>
2022-03-14 23:48:25 +0300

    Second Commit

--- HRManagement.UI/Controllers/HomeController.cs
+++ HRManagement.UI/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace HRManagement.UI.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        public int Enes { get; set; }
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
