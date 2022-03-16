fb999b3fab08eb9f5a478dc95e7688db945cafca
İnci Adıyaman <inci.adiyaman@hotmail.com>
2022-03-16 20:04:29 +0300

    İzinler entity classı ve enum classları oluşturuldu

---
+++ HRManagement.Model/Entities/Permission.cs
@@ -0,0 +1,22 @@
+﻿using HRManagement.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManagement.Model.Entities
+{
+    public class Permission
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; } //izin başlangıcı
+        public DateTime FinishDate { get; set; } //izin bitişi
+        public PermitStatus PermitStatus { get; set; }
+        public PermissionType PermissionType { get; set; }
+
+        public int EmployeeID { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
---
+++ HRManagement.Model/Enums/PermissionType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManagement.Model.Enums
+{
+    public enum PermissionType
+    {
+        YillikIzin = 1,
+        DogumIzni = 2,
+        BabalikIzni = 3,
+        EvlilikIzni=4,
+        HastalikIzni=5,
+        VefatIzni=6,
+        UcretsizIzin = 7,
+        TasinmaIzni=8,
+    }
+}
