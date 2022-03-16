96126a432474296ab5c18af7ba12771a9d4f2c1a
İnci Adıyaman <inci.adiyaman@hotmail.com>
2022-03-16 18:10:38 +0300

    employee entity eklendi

---
+++ HRManagement.Model/Entities/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManagement.Model.Entities
+{
+    public class Employee
+    {
+        public Employee()
+        {
+            //Permissions = new HashSet<Permission>();
+        }
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public DateTime StartDate { get; set; } 
+        public string Photo { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+
+        //public ICollection<Permission> Permissions { get; set; }
+    }
+}
--- HRManagement.UI/Startup.cs
+++ HRManagement.UI/Startup.cs
@@ -19,13 +19,13 @@ namespace HRManagement.UI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            //services.AddScopeBLL();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
