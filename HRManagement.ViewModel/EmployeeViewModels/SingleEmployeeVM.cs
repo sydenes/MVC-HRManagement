ce17cfa41b45d6cbae1917f941d4a0feaa8cce99
Kaan Lokum <kaanlokum@gmail.com>
2022-03-17 17:06:49 +0300

    SingleEmployeeVM propları yapıldı.

--- HRManagement.ViewModel/EmployeeViewModels/SingleEmployeeVM.cs
+++ HRManagement.ViewModel/EmployeeViewModels/SingleEmployeeVM.cs
@@ -8,5 +8,13 @@ namespace HRManagement.ViewModel.EmployeeViewModels
 {
     public class SingleEmployeeVM
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Photo { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
     }
 }
